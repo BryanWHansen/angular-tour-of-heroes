0c36c36a3857188297540b40bd152001919e8d63
BryanWHansen <bhansen1717@outlook.com>
2022-04-13 13:31:38 -0400

    Update hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -58,6 +58,7 @@ export class HeroService {
     )
   }
 
+<<<<<<< Updated upstream
   searchHero(term: string): Observable<Hero[]> {
     if (!term.trim()) {
       return of([]);
@@ -68,6 +69,21 @@ export class HeroService {
         this.log(`found heroes matching ${term}`) : this.log(`no heroes found matching ${term}`),
         catchError(this.handleError<Hero[]>('serachHeroes', []))
       )
+=======
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero with id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(id:number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+>>>>>>> Stashed changes
     );
   }
 
